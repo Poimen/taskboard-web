900f8ea91296b19c40b90d7b0d23f4374da572ab
Richard Shephard <richards@sahomeloans.com>
2019-12-10 22:04:04 +0200

    Add react-boostrap and start playing with login component

---
+++ src/pages/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className="flex">
+      <div className="flex-1 h-full bg-red-600">div 1</div>
+      <div className="flex-1 bg-green-400">div 2</div>
+    </div>
+  )
+}
+
+export default login
