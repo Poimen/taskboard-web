8c30e80707f1e19bfeeb298cc8e7c791d5868e32
Richard Shephard <richards@sahomeloans.com>
2020-01-03 21:47:51 +0200

    Initial login page before extract to components

--- src/layouts/landing/Landing.jsx
+++ src/layouts/landing/Landing.jsx
@@ -7,22 +7,25 @@ function Landing() {
   return (
     <div className="flex flex-row min-h-full">
       <div className="flex flex-1 items-center justify-center min-h-full bg-blue-500 flex-col">
-        <span className="tracking-widest text-white text-6xl font-hairline">Welcome to</span>
-        <span className="tracking-wide text-white text-6xl font-light">TASK | BOARD</span>
+        <span className="tracking-widest text-white text-6xl font-hairline">Welcome</span>
+        {/* <span className="tracking-wide text-white text-6xl font-light">TASK | BOARD</span> */}
       </div>
       <div className="flex flex-1.3 items-center justify-center min-h-full shadow-2xl shadow-outline bg-white">
         <div className="text-center">
           <span className="tracking-wider text-gray-700 text-4xl font-light">Login</span>
-          <div className="tracking-wider text-gray-700 text-4xl font-light mt-8 mr-16 ml-16">
+          <div className="tracking-wider text-gray-700 text-4xl font-light mt-8">
             <Input
+              size="large"
               placeholder="Enter username"
+              style={{ 'background-color': 'aliceblue' }}
               prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
             />
             <Input
+              size="large"
               placeholder="Enter password"
               prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
             />
-            <Button type="primary" style={{ width: '100%' }} htmlType="submit">Login</Button>
+            <Button type="primary" size="large" style={{ width: '100%' }} htmlType="submit">Login</Button>
           </div>
         </div>
       </div>
