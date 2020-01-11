2b1e5e22b620025146f66fafd87e3845420fed64
Richard Shephard <richards@sahomeloans.com>
2020-01-11 19:52:00 +0200

    Add icon specific mappings in webpack config

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,13 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
+const path = require('path');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css'
+  }),
+  addWebpackAlias({
+    '@ant-design/icons/lib/dist$': path.resolve(__dirname, './src/assets/icons.js')
   })
 );
---
+++ src/assets/icons.js
@@ -0,0 +1,7 @@
+export {
+  default as UserOutline
+} from '@ant-design/icons/lib/outline/UserOutline';
+
+export {
+  default as LockOutline
+} from '@ant-design/icons/lib/outline/LockOutline';
