88cdf2e6fa6e67ae3bcc35e6be9f57997149a878
Richard Shephard <richards@sahomeloans.com>
2020-01-15 22:07:01 +0200

    Theme initialisation

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,15 @@
-const { override, fixBabelImports, addWebpackAlias } = require('customize-cra');
+const { override, fixBabelImports, addWebpackAlias, addLessLoader } = require('customize-cra');
 const path = require('path');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css'
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { }
   }),
   addWebpackAlias({
     '@ant-design/icons/lib/dist$': path.resolve(__dirname, './src/assets/icons.js')
