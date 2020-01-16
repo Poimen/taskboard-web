be858f49b9459c932e36030259fd5d244f3a20de
Richard Shephard <richards@sahomeloans.com>
2020-01-16 22:22:35 +0200

    Reduce font sizes slightly

--- src/layouts/dashboard/Dashboard.jsx
+++ src/layouts/dashboard/Dashboard.jsx
@@ -74,8 +74,8 @@ function Dashboard() {
                     />
                   </div>
                   <div className="block w-auto flex text-right">
-                    <div className="-mt-1 mr-5 text-sm">
-                      <Badge count="3">
+                    <div className="-mt-1 mr-5 text-xs">
+                      <Badge count="3" style={{ 'font-size': 12 }}>
                         <div className="text-xl">
                           <Icon type="bell" />
                         </div>
@@ -86,7 +86,7 @@ function Dashboard() {
                     </div>
                     {/* change to dropdown component */}
                     <div className="block flex items-center ml-2">
-                      <span className="text-gray-600 text-sm mr-1">User Name</span>
+                      <span className="text-gray-600 text-xs mr-1">User Name</span>
                       <div>
                         <svg className="fill-current text-gray-600 h-4 w-4 block opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4.516 7.548c.436-.446 1.043-.481 1.576 0L10 11.295l3.908-3.747c.533-.481 1.141-.446 1.574 0 .436.445.408 1.197 0 1.615-.406.418-4.695 4.502-4.695 4.502a1.095 1.095 0 0 1-1.576 0S4.924 9.581 4.516 9.163c-.409-.418-.436-1.17 0-1.615z" /></svg>
                       </div>
