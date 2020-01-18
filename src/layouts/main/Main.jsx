a6240f9f14628052857f219d3341045a643942a6
Richard Shephard <richards@sahomeloans.com>
2020-01-18 22:50:10 +0200

    Hacky use of route to test inline display - nned to wire menu items to route now...

--- src/layouts/main/Main.jsx
+++ src/layouts/main/Main.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import './Main.css';
-import { Layout, Breadcrumb, Icon, Avatar, Input, Badge } from 'antd';
+import { Layout, Breadcrumb } from 'antd';
+import { Route, Switch } from 'react-router-dom';
 import SideMenu from './components/SideMenu';
+import MainHeader from './components/MainHeader';
+import Landing from 'layouts/landing/Landing';
 
-const { Header, Content } = Layout;
-const { Search } = Input;
+const { Content } = Layout;
 
 export default function Main() {
   return (
     <Layout className="min-h-screen">
       <SideMenu></SideMenu>
+      <Layout>
+        <MainHeader></MainHeader>
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+
+          <Switch>
+            <Route exact path='/dashboard' component={Landing} />
+          </Switch>
+
+        </Content>
+        {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
+      </Layout>
     </Layout>
   );
 }
