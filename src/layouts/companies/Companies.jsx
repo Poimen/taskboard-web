27c140f544f45ad16d9584b9d9e0e5f54b63d905
Richard Shephard <richards@sahomeloans.com>
2020-01-25 22:04:10 +0200

    Scaffold companies

--- src/layouts/companies/Companies.jsx
+++ src/layouts/companies/Companies.jsx
@@ -1,17 +1,67 @@
 import React from 'react';
-import { Table, Divider, Tag, Avatar, Button } from 'antd';
+import { Table, Tag, Button, Icon } from 'antd';
 
 function Companies() {
+  const columns = [
+    {
+      title: 'Company Name',
+      dataIndex: 'companyName',
+      key: 'companyName',
+      sorter: (a, b) => a.companyName.length - b.companyName.length
+    },
+    {
+      title: 'Assigned to',
+      dataIndex: 'assigned',
+      key: 'assigned',
+      sorter: (a, b) => a.assigned.length - b.assigned.length
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: tags => (
+        <span>
+          {tags.map(tag => {
+            const color = tag.length > 5 ? 'geekblue' : 'green';
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </span>
+      ),
+      sorter: (a, b) => a.tags[0].length - b.tags[0].length
+    }
+  ];
+
+  const data = [
+    {
+      key: '1',
+      companyName: 'Some company 1',
+      tags: ['bulk']
+    },
+    {
+      key: '2',
+      companyName: 'Some company 12',
+      tags: ['bulk']
+    },
+    {
+      key: '3',
+      companyName: 'Some company 123',
+      tags: ['container', 'rail']
+    }
+  ];
+
   return (
     <>
       <div className="mt-4 flex flex-row flex-auto mx-auto justify-between">
         <div>
-          <span className="text-3xl text-blue-600">Clients</span>
+          <span className="text-3xl text-blue-600">Companies</span>
         </div>
         <div className="flex items-center justify-end">
-          <Button icon="user-add" type="primary">
-            <span>Add contact</span>
-          </Button>
+          <Button className="mr-2">Filter <Icon type="filter"></Icon></Button>
+          <Button icon="usergroup-add" type="primary">Add Company</Button>
         </div>
       </div>
       <Table className="mt-4" columns={columns} dataSource={data} pagination={false} />
--- src/layouts/main/Main.jsx
+++ src/layouts/main/Main.jsx
@@ -7,6 +7,7 @@ import MainHeader from './components/MainHeader';
 import Dashboard from 'layouts/dashboard/Dashboard';
 import Tasks from 'layouts/tasks/Tasks';
 import Contacts from 'layouts/contacts/Contacts';
+import Companies from 'layouts/companies/Companies';
 import Calendar from 'layouts/calendar/Calendar';
 
 const { Content } = Layout;
@@ -22,6 +23,7 @@ export default function Main() {
             <Route exact path='/dashboard' component={Dashboard} />
             <Route exact path='/tasks' component={Tasks} />
             <Route exact path='/contact' component={Contacts} />
+            <Route exact path='/company' component={Companies} />
             <Route exact path='/calendar' component={Calendar} />
             <Route exact path='/knowledge' component={Calendar} />
             <Route exact path='/report' component={Calendar} />
