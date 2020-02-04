a5f8d92544710f71b7b7045abf1186f916329689
Richard Shephard <richards@sahomeloans.com>
2020-02-04 22:18:38 +0200

    Playing with layout styles

--- src/layouts/main/components/SideMenu.jsx
+++ src/layouts/main/components/SideMenu.jsx
@@ -20,48 +20,50 @@ function SideMenu() {
 
   return (
     <>
-      <Sider className="shadow-lg border-r-2 border-gray-300" collapsible theme="light" collapsed={sidebarCollapsed} onCollapse={() => setSidebarCollapse(prev => !prev)}>
+      <Sider className="" collapsible theme="light" collapsed={sidebarCollapsed} onCollapse={() => setSidebarCollapse(prev => !prev)}>
         <div className="logo flex items-center justify-center text-2xl">
           {LogoText(sidebarCollapsed)}
         </div>
-        <Menu theme="light" selectedKeys={[location.pathname]} mode="inline">
-          <Menu.Item key="/dashboard">
-            <SiderMenuOptionText icon="desktop" text="Dashboard"></SiderMenuOptionText>
-            <Link to="/dashboard" />
-          </Menu.Item>
-          <Menu.Item key="/tasks">
-            <SiderMenuOptionText icon="bars" text="Tasks"></SiderMenuOptionText>
-            <Link to="/tasks" />
-          </Menu.Item>
-          <Menu.Item key="/contact">
-            <SiderMenuOptionText icon="team" text="Contacts"></SiderMenuOptionText>
-            <Link to="/contact" />
-          </Menu.Item>
-          <Menu.Item key="/company">
-            <SiderMenuOptionText svg={Office} text="Companies"></SiderMenuOptionText>
-            <Link to="/company" />
-          </Menu.Item>
-          <Menu.Item key="/thirdparty">
-            <SiderMenuOptionText svg={Office} text="Third Parties"></SiderMenuOptionText>
-            <Link to="/thirdparty" />
-          </Menu.Item>
-          <Menu.Item key="/calendar">
-            <SiderMenuOptionText icon="calendar" text="Calendar"></SiderMenuOptionText>
-            <Link to="/calendar" />
-          </Menu.Item>
-          <Menu.Item key="/knowledge">
-            <SiderMenuOptionText icon="book" text="Knowledge Base"></SiderMenuOptionText>
-            <Link to="/knowledge" />
-          </Menu.Item>
-          <Menu.Item key="/report">
-            <SiderMenuOptionText icon="bar-chart" text="Reports"></SiderMenuOptionText>
-            <Link to="/report" />
-          </Menu.Item>
-          <Menu.Item key="/chat">
-            <SiderMenuOptionText svg={Chat} text="Chat"></SiderMenuOptionText>
-            <Link to="/chat" />
-          </Menu.Item>
-        </Menu>
+        <div className="border-r-2 border-gray-300">
+          <Menu theme="light" selectedKeys={[location.pathname]} mode="inline">
+            <Menu.Item key="/dashboard">
+              <SiderMenuOptionText icon="desktop" text="Dashboard"></SiderMenuOptionText>
+              <Link to="/dashboard" />
+            </Menu.Item>
+            <Menu.Item key="/tasks">
+              <SiderMenuOptionText icon="bars" text="Tasks"></SiderMenuOptionText>
+              <Link to="/tasks" />
+            </Menu.Item>
+            <Menu.Item key="/contact">
+              <SiderMenuOptionText icon="team" text="Contacts"></SiderMenuOptionText>
+              <Link to="/contact" />
+            </Menu.Item>
+            <Menu.Item key="/company">
+              <SiderMenuOptionText svg={Office} text="Companies"></SiderMenuOptionText>
+              <Link to="/company" />
+            </Menu.Item>
+            <Menu.Item key="/thirdparties">
+              <SiderMenuOptionText svg={Office} text="Third Parties"></SiderMenuOptionText>
+              <Link to="/thirdparties" />
+            </Menu.Item>
+            <Menu.Item key="/calendar">
+              <SiderMenuOptionText icon="calendar" text="Calendar"></SiderMenuOptionText>
+              <Link to="/calendar" />
+            </Menu.Item>
+            <Menu.Item key="/knowledge">
+              <SiderMenuOptionText icon="book" text="Knowledge Base"></SiderMenuOptionText>
+              <Link to="/knowledge" />
+            </Menu.Item>
+            <Menu.Item key="/report">
+              <SiderMenuOptionText icon="bar-chart" text="Reports"></SiderMenuOptionText>
+              <Link to="/report" />
+            </Menu.Item>
+            <Menu.Item key="/chat">
+              <SiderMenuOptionText svg={Chat} text="Chat"></SiderMenuOptionText>
+              <Link to="/chat" />
+            </Menu.Item>
+          </Menu>
+        </div>
       </Sider>
     </>
   );
