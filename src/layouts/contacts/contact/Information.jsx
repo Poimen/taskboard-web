1c0db9b26f354e0aa2aa0f0577601b1af7dbc989
Richard Shephard <richards@sahomeloans.com>
2020-02-08 22:21:47 +0200

    Add basic tasks to client

--- src/layouts/contacts/contact/Information.jsx
+++ src/layouts/contacts/contact/Information.jsx
@@ -10,31 +10,33 @@ function Information() {
   return (
     <>
       <Card className="w-1/3 pb-4" title="Details" extra={<Button type="dashed" shape="circle" icon="edit" />}>
-        <table class="w-full text-left table-fixed">
+        <table className="w-full text-left table-fixed">
           <thead>
-            <colgroup>
-              <col className="w-1/2"></col>
-              <col className="w-1/2"></col>
-            </colgroup>
           </thead>
-          <tbody class="align-baseline">
+          <colgroup>
+            <col className="w-1/3"></col>
+            <col className="w-2/3"></col>
+          </colgroup>
+          <tbody className="align-baseline">
             <tr>
-              <td class="p-4 border-b border-gray-300 whitespace-no-wrap">Email:</td>
-              <td class="p-4 border-b border-gray-300 break-words">example@sxample.com</td>
+              <td className="p-4 border-b border-gray-300 whitespace-no-wrap">Email:</td>
+              <td className="p-4 border-b border-gray-300 break-words">example@sxample.com</td>
             </tr>
             <tr>
-              <td class="p-4 border-b border-gray-300 whitespace-no-wrap">Phone:</td>
-              <td class="p-4 border-b border-gray-300">24872990234</td>
+              <td className="p-4 border-b border-gray-300 whitespace-no-wrap">Phone:</td>
+              <td className="p-4 border-b border-gray-300">24872990234</td>
             </tr>
             <tr>
-              <td class="p-4 border-b border-gray-300 whitespace-no-wrap">Address:</td>
-              <td class="p-4 border-b border-gray-300">42 Wallaby Way, Sydney, 4051, sdfkjhdskjfh</td>
+              <td className="p-4 border-b border-gray-300 whitespace-no-wrap">Address:</td>
+              <td className="p-4 border-b border-gray-300">42 Wallaby Way, Sydney, 4051, sdfkjhdskjfh</td>
             </tr>
             <tr>
               <td className="p-4 whitespace-no-wrap">
                 Tags:
                 <br />
-                + Add tag
+                <div className="text-blue-600">
+                  + Add tag
+                </div>
               </td>
               <td className="p-4 whitespace-pre">
                 <Tag color="geekblue" closable onClose={preventDefault} key="bulk">
--- src/layouts/contacts/contact/Tasks.jsx
+++ src/layouts/contacts/contact/Tasks.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Table } from 'antd';
 
 function Tasks() {
+  const columns = [
+    {
+      title: 'Task',
+      dataIndex: 'task',
+      key: 'task'
+    },
+    {
+      title: 'Due By',
+      dataIndex: 'dueBy',
+      key: 'dueBy'
+    },
+    {
+      title: 'Assigned To',
+      dataIndex: 'assingedTo',
+      key: 'assignedTo'
+    }
+  ];
+  const data = [
+    {
+      key: '1',
+      task: 'Something to do...',
+      dueBy: '12/12/2020',
+      assignedTo: 'AB'
+    }
+  ];
+
+  const history = useHistory();
+  const onRow = (history) => (record, rowIndex) => {
+    return {
+      onClick: event => {
+        console.log(`Clicked - ${JSON.stringify(record)}:${rowIndex}`);
+      }
+    };
+  };
+
   return (
     <>
-     Tasks....
+      <Table className="mt-4" columns={columns} dataSource={data} onRow={onRow(history)} />
     </>
   );
 }
