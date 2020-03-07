01b81f870dc0d1043b96488aedf6410c4ee0f4e7
Richard Shephard <richards@sahomeloans.com>
2020-03-07 21:26:37 +0200

    Update company details mock

--- src/layouts/companies/Companies.jsx
+++ src/layouts/companies/Companies.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Table, Tag, Button, Icon } from 'antd';
+import { Avatar, Card, Table, Tag, Button, Icon } from 'antd';
 import { useHistory } from 'react-router-dom';
+import Office from '@svg/Office';
 
 function Companies() {
   const columns = [
@@ -67,18 +68,26 @@ function Companies() {
 
   return (
     <>
-      <div className="p-3 flex flex-row flex-auto mx-auto justify-between bg-white border-b-2 border-gray-300">
-        <div>
-          <span className="text-xl text-blue-600">Companies</span>
-        </div>
-        <div className="flex items-center justify-end">
-          <Button className="mr-2">Filter <Icon type="filter"></Icon></Button>
-          <Button icon="usergroup-add" type="primary">Add Company</Button>
+      <div className="mx-4">
+        <div className="pt-3 pb-2 flex flex-row flex-auto mx-auto justify-between bg-white- border-b-2- border-gray-300">
+          <div>
+            <div className="flex items-center">
+              <Avatar shape="square" size={55} style={{ color: 'grey' }} icon={<Icon component={Office} className="font-base" />}></Avatar>
+              <div className="flex flex-col ml-2 text-2xl text-gray-800 font-medium">
+                <span className="text-xl text-blue-600">Companies</span>
+                <span className="text-gray-500 text-xs font-normal ml-1">77 companies</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-end justify-end">
+            <Button className="mr-2">Filter <Icon type="filter"></Icon></Button>
+            <Button icon="usergroup-add" type="primary">Add Company</Button>
+          </div>
         </div>
       </div>
       <div className="mr-4 ml-4 mt-8">
         {/* <Card className="mt-8 shadow"> */}
-          <Table onRow={onRow(history)} columns={columns} dataSource={data} pagination={false} />
+        <Table onRow={onRow(history)} columns={columns} dataSource={data} pagination={false} />
         {/* </Card> */}
       </div>
     </>
--- src/layouts/companies/company/Details.jsx
+++ src/layouts/companies/company/Details.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Breadcrumb, Icon } from 'antd';
+import { Avatar, Icon, Button } from 'antd';
+import Office from '@svg/Office';
 
 function Details() {
-
   return (
     <>
-      <div className="p-3 flex flex-row flex-auto mx-auto justify-between bg-white border-b-2 border-gray-300">
-        <div>
-          <Breadcrumb>
-            <Breadcrumb.Item>
-              <Icon className="text-blue-600" style={{ fontSize: 18 }} type="team"></Icon>
-              <span className="text-xl text-blue-600">Company</span>
-            </Breadcrumb.Item>
-            <Breadcrumb.Item>
-              <span className="text-xl text-blue-600">Boing Boing Transport</span>
-            </Breadcrumb.Item>
-          </Breadcrumb>
-          {/* <span className="text-xs text-blue-600"><Icon type="left"></Icon> Back to Clients</span> */}
+      <div className="mx-4">
+        <div className="pt-3 pb-2 flex flex-row flex-auto mx-auto justify-between bg-white- border-b-2- border-gray-300">
+          <div>
+            <div className="flex items-center">
+              <Avatar shape="square" size={55} style={{ color: 'grey' }} icon={<Icon component={Office} className="font-base" />}></Avatar>
+              <div className="flex flex-col ml-2 text-2xl text-gray-800 font-medium">
+                <div>
+                  <span className="text-xl text-blue-600">Company</span>
+                  <span className="mx-3 text-base text-gray-500">/</span>
+                  <span className="text-xl text-blue-600">Boing Boing Transport</span>
+                </div>
+                <span className="text-gray-500 text-xs font-normal ml-1">Transport company</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-end justify-end">
+          </div>
         </div>
+      </div>
+      <div className="mr-4 ml-4 mt-8">
       </div>
     </>
   );
