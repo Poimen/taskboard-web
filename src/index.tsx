afc2fcb169cc9106c779e4112891f61a5db9b1e5
Richard Shephard <richards@sahomeloans.com>
2020-03-20 21:39:47 +0200

    Initial redux state

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import './assets/generated/base.css';
+import store from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement);
---
+++ src/store/reducers/user.ts
@@ -0,0 +1,11 @@
+
+const initialState = {
+  user: {
+    loggedIn: false,
+    token: null
+  }
+};
+
+export function userReducer(state = initialState, action: any) {
+  return state;
+}
