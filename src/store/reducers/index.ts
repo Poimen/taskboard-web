d267099b976cc9e167224353ea96cb6b028a7e7c
Richard Shephard <richards@sahomeloans.com>
2020-03-21 19:04:41 +0200

    Linked store to header

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,4 +1,4 @@
 import { combineReducers } from 'redux';
 import { userReducer } from './user';
 
-export default combineReducers({ userReducer });
+export default combineReducers({ currentUser: userReducer });
--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -7,16 +7,20 @@ const initialState = {
 };
 
 export function userReducer(state = initialState, action: any) {
+  console.log(`state = ${JSON.stringify(state, null, 2)}`);
+  console.log(`action = ${JSON.stringify(action, null, 2)}`);
   switch (action.type) {
     case 'SET_USER':
       state = {
         ...state,
         loggedIn: true,
-        user: action.user,
+        user: action.userDetail,
         token: action.token
       };
       break;
   }
+
+  console.log(`after state = ${JSON.stringify(state, null, 2)}`);
 
   return state;
 }
